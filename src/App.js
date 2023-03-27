e9c27ff86b55ffe2f712dd26a546d340f7c2678b
JayeshW <jayeshwankhee28@gmail.com>
2023-03-28 01:47:40 +0530

    Initiate Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
-import Home from './components/Home';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+    <Header/>
     <Home/>
     </div>
   );
