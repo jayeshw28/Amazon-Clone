9cc22d90dfa1bcfc0b2c1ab088a52579307f7a1b
JayeshW <jayeshwankhee28@gmail.com>
2023-03-28 14:03:37 +0530

    branch carousel

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Carousel.scss'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+function Carousel() {
+    let settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    }
+
+  return (
+    <div className='carousel'>
+        <Slider {...settings} className='carousel-slider'>
+            <div><img className='carousel-slide-img' src='https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51O45Sl0WLL._SX3000_.jpg'/></div>
+            <div><img className='carousel-slide-img' src='https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61O759bS39L._SX3000_.jpg'/></div>
+            <div><img className='carousel-slide-img' src='https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51ovs76vmtL._SX3000_.jpg'/></div>
+            <div><img className='carousel-slide-img' src='https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61tjBFsRL+L._SX3000_.jpg'/></div>
+            <div><img className='carousel-slide-img' src='https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61JkX-1tN9L._SX3000_.jpg'/></div>
+        </Slider>
+        <div className='carousel-fade-div'></div>
+    </div>
+  )
+}
+
+export default Carousel
