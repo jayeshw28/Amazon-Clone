edbceb7921dc927f72b51008e6b48fac98c0cd6e
JayeshW <jayeshwankhee28@gmail.com>
2023-05-31 00:13:44 +0530

    aded stripe payment

--- src/components/CartPayments/CartPayment.js
+++ src/components/CartPayments/CartPayment.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CartPayment.scss";
 import LockIcon from "@mui/icons-material/Lock";
 import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
 import { useStateValue } from "../StateProvider/StateProvider";
-import Subtotal from "../Subtotal/Subtotal";
 import { Link } from "react-router-dom";
-import { UseStripe, UseElements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { UseStripe, UseElements, CardElement } from "@stripe/react-stripe-js";
+import StripeCheckout from "react-stripe-checkout";
+import { getCartTotal } from "../../reducer";
+
+// const promise = loadStripe(
+//   "pk_test_51N06I0SJxsVZXSFaCZ19Rw9JcaKNSAOeGeLjUyhh6eqXRNvEVKb5HLUKEoKehZFoAEVUv9TzcwwWZQnc4na3fanC00epz0ZJN3"
+// );
 
 function CartPayment() {
   const [{ cart, user }, dispatch] = useStateValue();
 
-  const stripe = UseStripe();
-  const elements = UseElements();
+  // const stripe = UseStripe();
+  // const elements = UseElements();
+
+  // const [error, setError] = useState(null);
+  // const [disabled, setDisabled] = useState(true);
+
+  // const HandleSubmit = (e) => {
+  //   e.preventDefault();
+  // };
+
+  // const HandleChange = (e) => {
+  //   setDisabled(e.empty);
+  //   setError(e.error ? e.error.message : "");
+  // };
+
+  const onToken = (token) => {
+    console.log(token);
+  };
 
   return (
     <div className="payment">
@@ -32,13 +54,14 @@ function CartPayment() {
                 <a>Delivery Address: </a>
 
                 <span>
-                  <span>Khushi Bangade</span>
+                  <span>Jayesh Wankhede</span>
                   <br />
-                  Matoshree Girls Hostel A(old), GECA Government College of
-                  Engineering Aurangabad,
+                  Hostel-6(Kabir Hostel), Indian Institute of Informartion
+                  Technology, Kota
                   <br />
-                  New Usmanpura <br />
-                  AURANGABAD, MAHARASHTRA 431005 India
+                  Jhalana Gram, Malviya Nagar
+                  <br />
+                  Jaipur, Rajasthan 302017 India
                   <br />
                   <span>Add Delivery Instructions</span>
                 </span>
@@ -80,6 +103,17 @@ function CartPayment() {
                 Select this option at checkout.
                 <br />
                 <a href="">Details</a>
+                <br />
+                <div className="payment-button-div">
+                  <StripeCheckout
+                    className="payment-button"
+                    stripeKey="pk_test_51N06I0SJxsVZXSFaCZ19Rw9JcaKNSAOeGeLjUyhh6eqXRNvEVKb5HLUKEoKehZFoAEVUv9TzcwwWZQnc4na3fanC00epz0ZJN3"
+                    token={onToken}
+                    amount={getCartTotal(cart) * 100}
+                    name="Amazon Clone"
+                    currency="INR"
+                  />
+                </div>
               </div>
             </div>
           </div>
